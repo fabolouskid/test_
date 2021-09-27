e3db4a6e96c3001083cb8416861dcf47c0ceaed1
fabolouskid <dj.fagbamila@gmail.com>
2021-09-27 09:45:03 -0400

    updated 2 files

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # hello
 
 # hi
+# Lets be civil here. No one is better than the other
+# we are all one
--- test.py
+++ test.py
@@ -1,1 +1,4 @@
 import pygame
+I came, i saw and conquered
+Dont try to play games with me
+I am not your mate. Feel me
